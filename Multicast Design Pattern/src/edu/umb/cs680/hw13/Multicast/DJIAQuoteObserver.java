8b184b53e48200e5b3e1042e997e106af0b54ff5
Praveenrtp <46226861+Praveenrtp@users.noreply.github.com>
2021-01-16 16:45:51 -0500

    Delete Multicast Design Pattern directory

--- Multicast Design Pattern/src/edu/umb/cs680/hw13/Multicast/DJIAQuoteObserver.java
+++
@@ -1,5 +0,0 @@
-package edu.umb.cs680.hw13.Multicast;
-
-public interface DJIAQuoteObserver {
-	public void updateDJIA(DJIAEvent obj);
-}
--- Multicast Design Pattern/src/edu/umb/cs680/hw13/Multicast/StockQuoteObserver.java
+++
@@ -1,5 +0,0 @@
-package edu.umb.cs680.hw13.Multicast;
-
-public interface StockQuoteObserver {
-	public void updateStock(StockEvent arg);
-}
